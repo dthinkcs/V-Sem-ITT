29650b0cb886c9cdc6c3698cd7c0ee6888c0cf77
rishabh <31166087+dthinkcs@users.noreply.github.com>
2019-09-24 20:51:04 +0530

    Update a1.py

--- ITT/LAB7/a1/a1.py
+++ ITT/LAB7/a1/a1.py
@@ -8,15 +8,15 @@ conn=sqlite3.connect('details.db')
 # conn.execute("INSERT INTO details VALUES('125', 'AJ', '76534', 'aj@gmail.com')")
 # conn.execute("INSERT INTO details VALUES('126', 'DT', '813265', 'd.t@gmail.com')")
 
-ph=raw_input("Enter phone number: ")
-em=raw_input("Enter email: ")
+ph = input("Enter phone number: ")
+em = input("Enter email: ")
 
-cursor=conn.execute("SELECT * FROM details")
-found=0
+cursor = conn.execute("SELECT * FROM details")
+found = 0
 for i in cursor:
 	if i[2]==ph and i[3]==em:
 		found=1
-		print "Hello",i[1]
+		print("Hello",i[1])
 		break
 if found==0:
 	print "Please check credentials and try again"
